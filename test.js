41d87f3367558a914e0180237b86d4b6d991dfd2
do-jonathan4 <d.johnt45@gmail.com>
2022-09-15 14:40:31 -0700

    module can change css color; removed console.log()

--- test.js
+++ test.js
@@ -1,10 +1,11 @@
 const fs = require('fs')
-const data = `body {
-    background-color: lightblue;
-  }`
+
 const hello = () => {
-    fs.writeFile('styles.css', data, () => {
-        console.log(hello)
-    })
+    const randomColor = Math.floor(Math.random()*16777215).toString(16)
+    // make sure to contrast text
+    const data = `body {
+        background-color: #${randomColor};
+      }`
+    fs.writeFile('styles.css', data, () => {})
 }
 hello()
